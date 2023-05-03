4fe968ecc6bc334a0ae76cc2520908190de1eb13
Catherine Mitchell <nicholscat@yahoo.com>
2023-05-03 20:12:13 -0300

    added HTTP Basic Authentication to the Admin dashboard and product page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @products = Product.order(id: :desc).all
   end
 end
