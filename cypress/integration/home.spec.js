5a01bfb23265df8b422afcbd86c914a1b6f19d1a
Catherine Mitchell <nicholscat@yahoo.com>
2023-05-09 10:33:26 -0300

    added cypress tests and modified the cypress intializer

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('Jungle app', () => {
+  it('displays the home page', () => {
+    
+    cy.visit('http://localhost:3000/')
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+})
